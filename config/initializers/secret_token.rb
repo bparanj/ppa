ff9f987545b2bdd337249d87ec4e2bfabcc58640
Bala Paranj <bparanj@gmail.com>
2013-02-09 19:02:04 -0800

    Readme with steps for PayPal Permissions API

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppa::Application.config.secret_token = '0b68c55a9c35ec71138ef2dab0b96eee79f7daa1ccb3821e53ac67d4fffa5c8079355fe65cab510e027d85e36ca785975fd0e77237dcb4dc1a88fab58d626389'
